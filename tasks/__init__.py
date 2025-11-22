a9200796d057c88d0baac3a579c1d18df1194314
Slava <slavik.troyan@gmail.com>
2025-11-22 19:23:05 +0100

    rename tasks

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -5,6 +5,6 @@ from config import huey
 # Import all tasks so they are registered with Huey
 # This is required when running: huey_consumer tasks.huey
 from . import code_generation_task  # noqa: F401
-from . import testing  # noqa: F401
+from . import test_task  # noqa: F401
 
 __all__ = ["huey"]
